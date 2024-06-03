bc9856c8248d2f57516173df0e49a5a9f3b8daef
Ramadhan <ramadanazam93@gmail.com>
2024-06-04 00:25:06 +0800

    replace main

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
